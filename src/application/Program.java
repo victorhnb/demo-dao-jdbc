d2ac2f0c03b2941d57ca70411f2eb027b3c4790e
Victor Hugo <victor_nascimento1996@hotmail.com>
2020-01-30 16:25:39 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		
+		
+		
+		Department obj = new Department(1, "Books");
+		Seller s = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(s);
 
 	}
 
